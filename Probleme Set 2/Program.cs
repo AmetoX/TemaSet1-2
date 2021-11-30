8b7d944d186a690bc11a76559385079478e4ec0d
AmetoX <xametox.officiall@yahoo.com>
2021-11-30 21:24:39 +0200

    1.1

--- Probleme Set 2/Program.cs
+++ Probleme Set 2/Program.cs
@@ -43,16 +43,22 @@ namespace Probleme_Set_2
                     set2p2();
                     break;
                 case 3:
+                    set2p3();
                     break;
                 case 4:
+                    set2p4();
                     break;
                 case 5:
+                    set2p5();
                     break;
                 case 6:
+                    set2p6();
                     break;
                 case 7:
+                    set2p7();
                     break;
                 case 8:
+                    set2p8();
                     break;
                 default:
                     break;
@@ -118,6 +124,152 @@ namespace Probleme_Set_2
                 sumgaus = sumgaus + i;
             }
             Console.WriteLine($"Suma numereleor de la 1 pana la {n} este: {sumgaus}.");
+        }
+        public static void set2p4()
+        {
+            Console.Write("Cate numere doriti sa aiba sirul? :");
+            int n = int.Parse(Console.ReadLine());
+            Console.Write("numarul pe care il cautati: ");
+            int a = int.Parse(Console.ReadLine());
+
+            StringBuilder sb = new StringBuilder();
+            sb.Capacity = n;
+            int b = 0;
+            bool ok = false;
+            for (int i = 0; i < n; i++)
+            {
+                int r = rnd.Next(1, 10);
+                //Console.Write(r + " ");
+                sb.Append(r);
+                b++;
+                if (r == a)
+                {
+
+                    Console.WriteLine($"am gasit nr {a} pe pozitia {b}");
+                    ok = true;
+                }
+                else
+                {
+
+                }
+            }
+            if (ok)
+            {
+
+            }
+            else
+            {
+                Console.WriteLine();
+                Console.WriteLine("-1");
+            }
+            Console.WriteLine("Sirul de numere generat: ");
+            for (int j = 0; j < sb.Length; j++)
+            {
+                Console.Write(sb[j] + " ");
+            }
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+        public static void set2p5()
+        {
+            Console.Write("Cate numere doriti sa aiba sirul? :");
+            int n = int.Parse(Console.ReadLine());
+            StringBuilder sb = new StringBuilder();
+            sb.Capacity = n;
+            int b = 0;
+            int a = 0;
+            for (int i = 0; i < n; i++)
+            {
+                int r = rnd.Next(1, 10);
+                //Console.Write(r + " ");
+                sb.Append(r);
+                if (r == b)
+                {
+                    a++;
+                }
+                else
+                {
+
+                }
+                b++;
+            }
+            for (int j = 0; j < sb.Length; j++)
+            {
+                Console.Write(sb[j] + " ");
+            }
+            Console.WriteLine();
+            for (int k = 0; k < sb.Length; k++)
+            {
+                Console.Write(k + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine($"Numere egale cu pozitia lor sunt: {a}");
+        }
+        public static void set2p6()
+        {
+           
+        }
+        public static void set2p7()
+        {
+            Console.Write("Cate numere doriti sa aiba sirul? :");
+            int n = int.Parse(Console.ReadLine());
+            StringBuilder sb = new StringBuilder();
+            sb.Capacity = n;
+            int m = 0;
+            int mic = 0;
+            int mic2 = -1;
+
+            for (int i = 0; i < n; i++)
+            {
+                int r = rnd.Next(1, 10);
+                //Console.Write(r + " ");
+                sb.Append(r);
+                if (r > m)
+                {
+                    m = r;
+                }
+                if (r == 0 || r == 1)
+                {
+                    mic2 = r;
+
+                }
+                else
+                {
+
+                }
+
+                if (r < m)
+                {
+                    if (r < mic)
+                    {
+                        mic = r;
+                    }
+                    else
+                    {
+
+                    }
+
+                }
+                else
+                {
+                    mic = r;
+                }
+
+            }
+            for (int j = 0; j < sb.Length; j++)
+            {
+                Console.Write(sb[j] + " ");
+            }
+            Console.WriteLine();
+            if (mic2 == 0 || mic2 == 1)
+            {
+                mic = mic2;
+            }
+            Console.WriteLine($"cel mai mare numar: {m}, si cel mai mic nr: {mic}.");
+        }
+        public static void set2p8()
+        {
+
         }
 
     }
