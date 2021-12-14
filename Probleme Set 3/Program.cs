b0208241419e0d3b76133da6dfb6d547eb3c92dd
AmetoX <xametox.officiall@yahoo.com>
2021-12-14 20:20:39 +0200

    3.9

--- Probleme Set 3/Program.cs
+++ Probleme Set 3/Program.cs
@@ -9,6 +9,7 @@ namespace Probleme_Set_3
 {
     public class Program
     {
+        static Random rnd = new Random();
         static void Main(string[] args)
         {
             //this will be empty for now...
@@ -40,10 +41,13 @@ namespace Probleme_Set_3
                 switch (set3)
                 {
                     case 1:
+                        set3p1();
                         break;
                     case 2:
+                        set3p2();
                         break;
                     case 3:
+                        set3p3();
                         break;
                     case 4:
                         break;
@@ -68,5 +72,114 @@ namespace Probleme_Set_3
                 goto begin;
             }
         }
+        public static void set3p1()
+        {
+            Console.Write("Cate numere doriti sa scrieti? : ");
+            int a = int.Parse(Console.ReadLine());
+            int[] arr;
+            arr = new int[a];
+            Console.Write("Introduceti numerele unu dupa altul cu un spatiu intr-e ele: ");
+            string b = Console.ReadLine();
+            string[] local_data = b.Split(' ');
+            for (int i = 0; i < a; i++)
+            {
+                int c = int.Parse(local_data[i]);
+                arr[i] = c;
+            }
+            int suma = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                suma = arr[i] + suma;
+            }
+            PrintArray(arr);
+            Console.WriteLine();
+            Console.WriteLine($"Suma sirului introdus este: {suma}");
+        }
+        public static void set3p2()
+        {
+            Console.Write("Cate numere doriti sa contina vectorul?: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Scrieti nr pe care il cautati(intr-e -100 si 100): ");
+            int b = int.Parse(Console.ReadLine());
+            int c = 0;
+            bool ok = false;
+            int[] arr;
+            arr = new int[a];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rnd.Next(-100, 100);
+            }
+            for (int i = 0; i < arr.Length; i++)
+            {
+
+                if (arr[i] == b)
+                {
+                    c = i;
+                    ok = true;
+                }
+
+            }
+            PrintArray(arr);
+            Console.WriteLine();
+            if (ok)
+            {
+                Console.WriteLine($"Numarul cautat({b}) a fost gasit si este pe pozita: {c}");
+            }
+            else
+            {
+                Console.WriteLine($"Numarul {b} nu se afla in acest sir.");
+            }
+
+        }
+        public static void set3p3()
+        {
+            Console.Write("Cate numere doriti sa contina vecotrul? : ");
+            int a = int.Parse(Console.ReadLine());
+            int[] arr;
+            arr = new int[a];
+            int[] arr2 = new int[a];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rnd.Next(-100, 100);
+            }
+            int b = -1;
+            int c = 0;
+            int d = 0;
+            int e = 0;
+            int f = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr2[i] = arr[i];
+            }
+            Array.Sort(arr2);
+            for (int i = 0; i < arr.Length; i++)
+            {
+                b++;
+                if (arr[i] == arr2[a - 1])
+                {
+                    c = arr[i];
+                    d = b;
+                }
+                if (arr[i] == arr2[0])
+                {
+                    e = arr[i];
+                    f = b;
+                }
+            }
+
+            PrintArray(arr);
+            Console.WriteLine();
+            Console.WriteLine($"Cel mai mare nr este: {c} pe pozitia {d}");
+            Console.WriteLine($"Cel mai mic nr este: {e} pe pozitia {f}");
+        }
+        private static void PrintArray(int[] arr)
+        {
+            Console.Write("Vectorul: ");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+        }
     }
+    
 }
--- TestArea/Program.cs
+++ TestArea/Program.cs
@@ -13,7 +13,55 @@ namespace TestArea
         static Random rnd = new Random();
         static void Main(string[] args)
         {
+            Console.Write("Cate numere doriti sa contina vectorul?: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.Write("Scrieti nr pe care il cautati(intr-e -100 si 100): ");
+            int b = int.Parse(Console.ReadLine());
+            int c = 0;
+            bool ok = false;
+            int[] arr;
+            arr = new int[a];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rnd.Next(-100, 100);
+            }
+            for (int i = 0; i < arr.Length; i++)
+            {
+                
+                if(arr[i]==b)
+                {
+                    c = i;
+                    ok = true;
+                }
 
+            }
+            
+            PrintArray(arr);
+            Console.WriteLine();
+            if (ok)
+            {
+                Console.WriteLine($"Numarul cautat({b}) a fost gasit si este pe pozita: {c}");
+            }
+            else
+            {
+                Console.WriteLine($"Numarul {b} nu se afla in acest sir.");
+            }
+        }       
+        private static void PrintArray(int[] arr)
+        {
+            Console.Write("Vectorul: ");
+            for (int i = 0; i<arr.Length; i++)
+            {
+                Console.Write(arr[i]+" ");
+            }
+        }
+        private static int[] GenerateArray(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rnd.Next(-100, 100);
+            }
+            return arr;
         }
     }
 }
