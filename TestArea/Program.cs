9064282f858ba25c91e55db07e9c2d85e30cdc93
AmetoX <xametox.officiall@yahoo.com>
2022-01-06 14:53:47 +0200

    7.5

--- TestArea/Program.cs
+++ TestArea/Program.cs
@@ -21,10 +21,65 @@ namespace TestArea
                 arr[i] = rnd.Next(0, 100);
             }
             PrintArray(arr);
-            QuickSort(arr, 0, arr.Length);
             Console.WriteLine();
+            Console.WriteLine("Sorting...");
+            MergeSort(arr,0,arr.Length-1);
+            Console.WriteLine();
+            Console.WriteLine("-Merge sort-");
             PrintArray(arr);
             Console.WriteLine();
+        }
+        static void Merge(int[] array, int lowIndex, int middleIndex, int highIndex)
+        {
+            var left = lowIndex;
+            var right = middleIndex + 1;
+            var tempArray = new int[highIndex - lowIndex + 1];
+            var index = 0;
+
+            while ((left <= middleIndex) && (right <= highIndex))
+            {
+                if (array[left] < array[right])
+                {
+                    tempArray[index] = array[left];
+                    left++;
+                }
+                else
+                {
+                    tempArray[index] = array[right];
+                    right++;
+                }
+
+                index++;
+            }
+
+            for (var i = left; i <= middleIndex; i++)
+            {
+                tempArray[index] = array[i];
+                index++;
+            }
+
+            for (var i = right; i <= highIndex; i++)
+            {
+                tempArray[index] = array[i];
+                index++;
+            }
+
+            for (var i = 0; i < tempArray.Length; i++)
+            {
+                array[lowIndex + i] = tempArray[i];
+            }
+        }
+        static int[] MergeSort(int[] array, int lowIndex, int highIndex)
+        {
+            if (lowIndex < highIndex)
+            {
+                var middleIndex = (lowIndex + highIndex) / 2;
+                MergeSort(array, lowIndex, middleIndex);
+                MergeSort(array, middleIndex + 1, highIndex);
+                Merge(array, lowIndex, middleIndex, highIndex);
+            }
+
+            return array;
         }
         public static int[] QuickSort(int[]arr, int start, int end)
         {
